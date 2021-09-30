3abe864f1b24a341f4f974ebb0eead5e7c200630
Jaydip Patel <48186367+JaydipPatel2009@users.noreply.github.com>
2021-09-30 15:34:58 -0700

    Update BaseTest.java

--- ExtentSparkReport/src/main/java/runner/BaseTest.java
+++ ExtentSparkReport/src/main/java/runner/BaseTest.java
@@ -11,24 +11,32 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class BaseTest {
-	public static WebDriver driver;
-	static ExtentReports extent;
+		public static WebDriver driver;
+	static Wait<WebDriver> wait;
 	@BeforeTest
 	public static void setUp() throws IOException {
-//		System.out.println(System.getProperty("user.dir"));
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/chromedriver");
-		driver = new ChromeDriver();
-		System.out.println("Driver successfully started...");
-//		String reportPath = System.getProperty("user.dir")+"/target/test-output/Spark/Spark.html";
-//		ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
-//		String reportConfigPath = System.getProperty("user.dir")+"/src/test/resources/extent-config.xml";
-//		reporter.loadXMLConfig(reportConfigPath);
-//		
-//		//report for attachment
-//		extent = new ExtentReports();
-//		extent.setAnalysisStrategy(AnalysisStrategy.TEST);
-//		extent.attachReporter(reporter);
+//		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/chromedriver");
+//		driver = new ChromeDriver();
 		
+		
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("profile.managed_default_content_settings.notifications", 1);
+		
+		//Create chrome options to set this prefs
+		ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("prefs", prefs);
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		wait = new FluentWait<WebDriver>(driver)
+			       .withTimeout(30, TimeUnit.SECONDS)
+			       .pollingEvery(5, TimeUnit.SECONDS)
+			       .ignoring(NoSuchElementException.class);
+		
+		
+		System.out.println("Driver successfully started...");
 	}
 	@AfterTest
 	public static void after() {
