2b16c491ed0f9961302696a74fe4934d60d9267d
Jaydip Patel <48186367+JaydipPatel2009@users.noreply.github.com>
2021-09-30 15:36:56 -0700

    Update BaseTest.java

--- ExtentSparkReport/src/main/java/runner/BaseTest.java
+++ ExtentSparkReport/src/main/java/runner/BaseTest.java
@@ -12,7 +12,7 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class BaseTest {
 		public static WebDriver driver;
-	static Wait<WebDriver> wait;
+// 	static Wait<WebDriver> wait;
 	@BeforeTest
 	public static void setUp() throws IOException {
 //		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/chromedriver");
@@ -30,10 +30,10 @@ public class BaseTest {
 		driver = new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		wait = new FluentWait<WebDriver>(driver)
-			       .withTimeout(30, TimeUnit.SECONDS)
-			       .pollingEvery(5, TimeUnit.SECONDS)
-			       .ignoring(NoSuchElementException.class);
+// 		wait = new FluentWait<WebDriver>(driver)
+// 			       .withTimeout(30, TimeUnit.SECONDS)
+// 			       .pollingEvery(5, TimeUnit.SECONDS)
+// 			       .ignoring(NoSuchElementException.class);
 		
 		
 		System.out.println("Driver successfully started...");
